6c679c32e4de09a64c3c74112a4e5b344499c507
Kelly Zhou <yinglinzhou0@gmail.com>
2023-05-07 22:27:22 -0400

    move lives

---
+++ src/scripts/lives.js
@@ -0,0 +1,22 @@
+// function heart() {
+//     if (a.lives === 3) {
+//         ctx.drawImage(redheart, 0, 0, 32, 32, 620, 15, 32, 32)
+//         ctx.drawImage(redheart, 0, 0, 32, 32, 585, 15, 32, 32)
+//         ctx.drawImage(redheart, 0, 0, 32, 32, 550, 15, 32, 32)
+//     }
+//     if (a.lives === 2) {
+//         ctx.drawImage(emptyheart, 0, 0, 32, 32, 620, 15, 32, 32)
+//         ctx.drawImage(redheart, 0, 0, 32, 32, 585, 15, 32, 32)
+//         ctx.drawImage(redheart, 0, 0, 32, 32, 550, 15, 32, 32)
+//     }
+//     if (a.lives === 1) {
+//         ctx.drawImage(emptyheart, 0, 0, 32, 32, 620, 15, 32, 32)
+//         ctx.drawImage(emptyheart, 0, 0, 32, 32, 585, 15, 32, 32)
+//         ctx.drawImage(redheart, 0, 0, 32, 32, 550, 15, 32, 32)
+//     }
+//     if (a.lives === 0) {
+//         ctx.drawImage(emptyheart, 0, 0, 32, 32, 620, 15, 32, 32)
+//         ctx.drawImage(emptyheart, 0, 0, 32, 32, 585, 15, 32, 32)
+//         ctx.drawImage(emptyheart, 0, 0, 32, 32, 550, 15, 32, 32)
+//     }
+// }
